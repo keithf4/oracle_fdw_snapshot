79c09a538c00bfe35aa05bf9f571647195182a1d
Keith Fiske <keith@keithf4.com>
2012-01-28 18:30:48 -0500

    Qualify schema in table setup

--- setup.sql
+++ setup.sql
@@ -1,6 +1,6 @@
 CREATE SCHEMA ofdw;
 
-CREATE TABLE oracle_tbltranslation (
+CREATE TABLE ofdw.oracle_tbltranslation (
     src_table text CONSTRAINT pk_oracle_tbltranslation PRIMARY KEY, 
     dest_table text UNIQUE NOT NULL, 
     fdw_table text UNIQUE NOT NULL
