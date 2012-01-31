2ecb768356628730f3de7ec9eebe07059a9de9de
Keith Fiske <keith@keithf4.com>
2012-01-31 11:26:43 -0500

    Updated setup files. Created README

--- setup_pg.sql
+++ setup_pg.sql
@@ -5,3 +5,14 @@ CREATE TABLE ofdw.oracle_tbltranslation (
     dest_table text UNIQUE NOT NULL, 
     fdw_table text UNIQUE NOT NULL
 );
+
+CREATE FOREIGN TABLE ofdw.oracle_table_columns (
+    owner text, 
+    table_name text,
+    column_name text,
+    data_type text,
+    data_length numeric,
+    column_id numeric
+) SERVER your_server_name
+OPTIONS (schema 'your_oracle_schema', table 'PG_FDW_TABLE_COLUMNS');
+
