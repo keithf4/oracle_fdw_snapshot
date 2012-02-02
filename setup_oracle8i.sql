0cadf974e4f218f2d150b456add4ff2e6ac963e7
Keith Fiske <keith@keithf4.com>
2012-02-02 17:29:37 -0500

    Changed oracle view to use all_tab_columns instead of dba sys table. Updated README

--- setup_oracle8i.sql
+++ setup_oracle8i.sql
@@ -1,1 +1,1 @@
-CREATE VIEW PG_FDW_TABLE_COLUMNS AS select owner, table_name, column_name, data_type, data_length, column_id from dba_tab_columns;
+CREATE OR REPLACE VIEW PG_FDW_TABLE_COLUMNS AS SELECT owner, table_name, column_name, data_type, data_length, column_id FROM all_tab_columns;
